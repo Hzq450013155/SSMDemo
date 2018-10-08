5aa3f20bdb7d2ea71f1f08cf738feb485ef44ca4
haozongqi <zongqi.hao@hand-china.com>
2018-10-08 19:10:07 +0800

    [UPDATE] 面向切面两种AOP编程实例

---
+++ src/main/java/com/ssm/aoptest/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ssm.aoptest;
+
+/**
+ * @author zongqi.hao@hand-china.com
+ * @version 1.0
+ * @name HelloWorld
+ * @description ${DESCRIPTION}
+ * @date 2018-09-28
+ */
+public interface HelloWorld {
+
+    void printHelloWorld();
+
+    void doPrint();
+}
---
+++ src/main/java/com/ssm/aoptest/TimeHander.java
@@ -0,0 +1,15 @@
+package com.ssm.aoptest;
+
+/**
+ * @author zongqi.hao@hand-china.com
+ * @version 1.0
+ * @name TimeHander
+ * @description 横切关注点
+ * @date 2018-09-28
+ */
+public class TimeHander {
+
+    public void printTime() {
+        System.out.println("CurrentTime = " + System.currentTimeMillis());
+    }
+}
